83254a86b0df7b5007943d683903e62d15cf0f05
Valentín García <htmgarcia@gmail.com>
2020-05-22 18:18:24 -0500

    Introduce bg parameter

--- src/functions.php
+++ src/functions.php
@@ -9,3 +9,28 @@
 
 // Restrict Access to within Joomla
 defined('_JEXEC') or die('Restricted access');
+
+
+$background = $this->params->get('background', 2);
+
+if($background == 1) {
+
+    // The home background will be used in all pages
+    JFactory::getDocument()->addStyleDeclaration(
+        'body, body.home {
+            background-image: url(' . Juri::base() . 'templates/js_breezy/images/' . $this->params->get('style', 'green') . '/bg-home.png);
+        }'
+    );
+
+} elseif($background == 0) {
+
+    // No image background in all pages, including home
+    JFactory::getDocument()->addStyleDeclaration(
+        'body, body.home {
+            background-image: none;
+        }'
+    );
+} else {
+
+    // Nothing to do here
+}
