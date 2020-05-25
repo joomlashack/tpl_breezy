181f99993b89782253cf128c6c2f11c1fedb2ac4
Valentín García <htmgarcia@gmail.com>
2020-05-25 17:41:26 -0500

    Disabled option

--- src/functions.php
+++ src/functions.php
@@ -13,18 +13,32 @@ defined('_JEXEC') or die('Restricted access');
 
 $background = $this->params->get('background', 'home');
 $style      = $this->params->get('style', 'green');
+$app        = JFactory::getApplication();
+
+if ($style == 'custom') {
+    $style = 'green';
+}
 
 if($background == 'all') {
 
-    if ($style == 'custom') {
-        $style = 'green';
-    }
-
     // The home background will be used in all pages
     JFactory::getDocument()->addStyleDeclaration(
-        'body {
-            background-image: url(' . Juri::base() . 'templates/js_breezy/images/' . $style . '/bg-home.png);
+        'body, body.home {
+            background-image: url(' . Juri::base() . 'templates/' . $app->getTemplate() . '/images/' . $style . '/bg-home.png);
         }'
     );
+
+} elseif($background == '0') {
+
+    // The default background will be used in all pages
+    JFactory::getDocument()->addStyleDeclaration(
+        'body, body.home {
+            background-image: url(' . Juri::base() . 'templates/' . $app->getTemplate() . '/images/' . $style . '/bg-default.png);
+        }'
+    );
+
+} else {
+
+    // Nothing to do here
 
 }
